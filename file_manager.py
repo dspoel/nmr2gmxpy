e33bcb87ff63f04572e26d02d461ab6d409dc8a3
David van der Spoel <spoel@xray.bmc.uu.se>
2020-05-04 22:25:34 +0200

    Turned FileManager into a class.
    TODO: Integrate into nmr2gmx.py

--- file_manager.py
+++ file_manager.py
@@ -42,32 +42,7 @@ from datetime import datetime
 # will be set to true when/if the dir is created by this script
 DELETE_DIR_RIGHT = False
 
-#========================COMMAND LINE ARGUMENTS PARSING==========================
-# When command line argument parser error occure, the program print --help
-class My_argument_parser(argparse.ArgumentParser):
-    def error(self, message):
-        sys.stderr.write('error: %s\n' % message)
-        self.print_help()
-        sys.exit(2)
-    
-def parse_arguments():
-    #parser = argparse.ArgumentParser()
-    parser = My_argument_parser(description=__doc__,
-                            formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-n", "--name", help = "Conventional 4-symbol name for the protein.",
-                         type=str, required=True)
-    parser.add_argument("-v", "--verbose", help="Print information as we go", action="store_true")
-    FORCE_FIELD = "amber99sb-ildn"
-    parser.add_argument("-ff", "--force_field", help="Force field to use. Only AMBER variants will work for now", default=FORCE_FIELD)
-    WATER_MODEL = "tip3p"
-    parser.add_argument("-water", "--water_model", help="Water model to use. Only AMBER variants will work for now", default=WATER_MODEL)
-    parser.add_argument("-gmx", "--run_gromacs", help="GROMACS will be called to generate topology file", action="store_true")
-    args = parser.parse_args()
-    
-    return args
-
-#================================================================================
-
+# Low-level routines
 def unzip(file_in, file_out, VERBOSE):
     if VERBOSE:
         print("Try to unzip %s"%file_in)
@@ -106,8 +81,6 @@ def download(ftp, folder_name, file_in, file_out, VERBOSE, protein):
         print("ERROR:")
         print(ex)
         sys.exit(1)
-
-
         
 def which(program):
     def is_exe(fpath):
@@ -133,112 +106,133 @@ def find_gmx():
                 return gmx
     return None
 
-def gromacs_command_line(protein, top_file, gro_file, VERBOSE, FORCE_FIELD, WATER_MODEL):
-    gmx = find_gmx()
-    if not gmx:
-        print("Can not find a GROMACS executable. Stopping here.")
-        exit(1)
 
-    command_line = gmx + " pdb2gmx"
-    command_line += " -f " + protein + ".pdb" + " -p " + top_file + " -o " + gro_file
-    command_line += " -ff " + FORCE_FIELD + " -water " + WATER_MODEL + " -ignh -merge all "
-    
-    if not VERBOSE:
-        command_line += " > /dev/null 2>&1"
-    return command_line
-    
-def download_and_unzip(protein, log):
-    # Connect to RCSB PDB (US) ftp server
-    SERVER_NAME = 'ftp.rcsb.org'
-    if VERBOSE:
-        print("Trying to connect to the server %s"%SERVER_NAME)
-        print("Server message:=================================")
-    try:
-        ftp = ftplib.FTP(SERVER_NAME)
-        message = ftp.login(user='anonymous', passwd='', acct='')
-        if VERBOSE:
-            print(message)
-        message = ftp.getwelcome()
-        if VERBOSE:
-            print(message)
-    except Exception as ex:
-        print("ERROR:")
-        print(ex)
-        sys.exit(1)
-    if VERBOSE:
-        print("================================================")
+#========================COMMAND LINE ARGUMENTS PARSING==========================
+# When command line argument parser error occure, the program print --help
+class My_argument_parser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
 
-    # on ftp server they use lower case names
-    protein_lowcase = protein.lower()
-    subfolder = protein_lowcase[1:-1] # two middle characters
+class FileManager():
+#    def __init__(self):
+        # Do nothing
+    def addArguments(self, parser):
+        parser.add_argument("-n", "--protein", help = "Conventional 4-symbol name for the protein.",
+                            type=str, required=True)
+        parser.add_argument("-v", "--verbose", help="Print information as we go", action="store_true")
+        FORCE_FIELD = "amber99sb-ildn"
+        parser.add_argument("-ff", "--force_field", help="Force field to use. Only AMBER variants will work for now", default=FORCE_FIELD)
+        WATER_MODEL = "tip3p"
+        parser.add_argument("-water", "--water_model", help="Water model to use. Only AMBER variants will work for now", default=WATER_MODEL)
+        parser.add_argument("-gmx", "--run_gromacs", help="GROMACS will be called to generate topology file", action="store_true")
 
-    # Download NMR data
-    folder_str = "/pub/pdb/data/structures/divided/nmr_restraints_v2/" + subfolder
-    remote_file_strgz = protein_lowcase + "_mr.str.gz"
-    file_strgz = protein_lowcase + "_mr.str.gz"
-    file_str = protein + "_mr.str"
-    download(ftp, folder_str, remote_file_strgz, file_strgz, VERBOSE, protein)
-    unzip(file_strgz, file_str, VERBOSE)
-    if VERBOSE:
-        print("SUCCESS")
-    os.remove(file_strgz)
+    def parseArguments(self, parser):
+        self.args = parser.parse_args()
 
-    # Download pdb file
-    folder_pdb = "/pub/pdb/data/structures/divided/pdb/" + subfolder
-    remote_file_pdbgz = "pdb" + protein_lowcase + ".ent.gz"
-    file_pdbgz = "pdb" + protein_lowcase + ".ent.gz"
-    file_pdb = protein + ".pdb"
-    download(ftp, folder_pdb, remote_file_pdbgz, file_pdbgz, VERBOSE, protein)
-    unzip(file_pdbgz, file_pdb, VERBOSE)
-    if VERBOSE:
-        print("SUCCESS")
-    os.remove(file_pdbgz)
-    
-    log.write("\nThe data has been downloaded from the server: %s.\n\n"%SERVER_NAME)
-    
-if __name__ == "__main__":
-    args = parse_arguments()
-    VERBOSE = args.verbose
+    def gromacsCommandLine(self, top_file, gro_file):
+        gmx = find_gmx()
+        if not gmx:
+            print("Can not find a GROMACS executable. Stopping here.")
+            exit(1)
 
-    protein = args.name
+        command_line = gmx + " pdb2gmx" + " -f " + self.args.protein + ".pdb" + " -p " + top_file + " -o " + gro_file + " -ff " + self.args.force_field + " -water " + self.args.water_model + " -ignh -merge all "
+    
+        if not self.args.verbose:
+            command_line += " > /dev/null 2>&1"
+        return command_line
 
-    # Create directory (if not exist) where to download the files
-    if not os.path.exists(protein):
-        DELETE_DIR_RIGHT = True
-    os.makedirs(protein, exist_ok="True")
-    os.chdir(protein)
-    
-    logfile = "file_manager.log"
-    log = open(logfile, "w")
-    log.write(datetime.now().strftime("On %d %B %Y at %H:%M:%S"))
-    log.write("\n-----------------------------\n")
-    
-    download_and_unzip(protein, log)
-    
-    
-#--------------------------CALL GROMACS-------------------------------------------------
-    
-    if args.run_gromacs:
-        file_top = protein + ".top"
-        file_gro = protein + "_clean.pdb"
-        command_line = gromacs_command_line(protein, file_top, file_gro, VERBOSE,
-                                            args.force_field, args.water_model)
+    def downloadAndUnzip(self, log):
+        # Connect to RCSB PDB (US) ftp server
+        SERVER_NAME = 'ftp.rcsb.org'
+        if self.args.verbose:
+            print("Trying to connect to the server %s" % SERVER_NAME)
+            print("Server message:=================================")
         try:
-            if VERBOSE:
-                print("Try to run:\n\t" + command_line)
-                print("============= GROMACS output: ==============================================")
-            errno = os.system(command_line)
-            #print("error number ", errno)
-            # GROMACS ERROR
-            if errno > 0:
-                raise Exception("ERROR: GROMACS had a problem. Run with -v to get more info.");
-
-            if VERBOSE:
-                print("============= END of GROMACS output ========================================")
-                print("SUCCESS")
-                
-            log.write("Call for GROMACS:\n");
-            log.write(command_line);
+            ftp = ftplib.FTP(SERVER_NAME)
+            message = ftp.login(user='anonymous', passwd='', acct='')
+            if self.args.verbose:
+                print(message)
+            message = ftp.getwelcome()
+            if self.args.verbose:
+                print(message)
         except Exception as ex:
+            print("ERROR:")
             print(ex)
-    log.close()
+            sys.exit(1)
+        if self.args.verbose:
+            print("================================================")
+
+        # on ftp server they use lower case names
+        protein_lowcase = self.args.protein.lower()
+        subfolder = protein_lowcase[1:-1] # two middle characters
+
+        # Download NMR data
+        folder_str = "/pub/pdb/data/structures/divided/nmr_restraints_v2/" + subfolder
+        remote_file_strgz = protein_lowcase + "_mr.str.gz"
+        file_strgz = protein_lowcase + "_mr.str.gz"
+        file_str = self.args.protein + "_mr.str"
+        download(ftp, folder_str, remote_file_strgz, file_strgz, self.args.verbose, self.args.protein)
+        unzip(file_strgz, file_str, self.args.verbose)
+        if self.args.verbose:
+            print("SUCCESS")
+        os.remove(file_strgz)
+
+        # Download pdb file
+        folder_pdb = "/pub/pdb/data/structures/divided/pdb/" + subfolder
+        remote_file_pdbgz = "pdb" + protein_lowcase + ".ent.gz"
+        file_pdbgz = "pdb" + protein_lowcase + ".ent.gz"
+        file_pdb = self.args.protein + ".pdb"
+        download(ftp, folder_pdb, remote_file_pdbgz, file_pdbgz, self.args.verbose, self.args.protein)
+        unzip(file_pdbgz, file_pdb, self.args.verbose)
+        if self.args.verbose:
+            print("SUCCESS")
+        os.remove(file_pdbgz)
+    
+        log.write("\nThe data has been downloaded from the server: %s.\n\n"%SERVER_NAME)
+
+    def makeWorkingDirectory(self):
+        # Create directory (if not exist) where to download the files
+        if not os.path.exists(self.args.protein):
+            DELETE_DIR_RIGHT = True
+        os.makedirs(self.args.protein, exist_ok="True")
+        os.chdir(self.args.protein)
+
+    def runGromacs(self, log):
+        #------CALL GROMACS-------------------------------------------------
+        if self.args.run_gromacs:
+            file_top = self.args.protein + ".top"
+            file_gro = self.args.protein + "_clean.pdb"
+            command_line = self.gromacsCommandLine(file_top, file_gro)
+            try:
+                if self.args.verbose:
+                    print("Try to run:\n\t" + command_line)
+                    print("============= GROMACS output: ==============================================")
+                errno = os.system(command_line)
+                #print("error number ", errno)
+                # GROMACS ERROR
+                if errno > 0:
+                    raise Exception("ERROR: GROMACS had a problem. Run with -v to get more info.");
+
+                if self.args.verbose:
+                    print("============= END of GROMACS output ========================================")
+                    print("SUCCESS")
+                
+                log.write("Call for GROMACS:\n");
+                log.write(command_line);
+            except Exception as ex:
+                print(ex)
+        
+if __name__ == "__main__":
+    parser  = My_argument_parser(description=__doc__,
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+    with open("file_manager.log", "w") as log:
+        log.write(datetime.now().strftime("On %d %B %Y at %H:%M:%S"))
+        log.write("\n-----------------------------\n")
+        manager = FileManager()
+        manager.addArguments(parser)
+        manager.parseArguments(parser)
+        manager.makeWorkingDirectory()
+        manager.downloadAndUnzip(log)
+        manager.runGromacs(log)
